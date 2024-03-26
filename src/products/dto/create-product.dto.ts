199b42d3258de47867f2a22358ef6a129e9a08df
Gabriel Barbarin Gorostegui <gabriel.barbarin@clouddistrict.com>
2024-03-26 10:45:59 +0100

    [ADD] useGlobalPipes and envs with joi

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,16 @@
-export class CreateProductDto {}
+import { Type } from 'class-transformer';
+import { IsNotEmpty, IsNumber, IsString, Min } from 'class-validator';
+
+export class CreateProductDto {
+  @IsNotEmpty()
+  @IsString()
+  public name: string;
+
+  @IsNotEmpty()
+  @IsNumber({
+    maxDecimalPlaces: 4,
+  })
+  @Min(0)
+  @Type(() => Number)
+  public price: number;
+}
