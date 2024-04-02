03c8c22fa5a2c0d302d6e97e6a5d13981d00c229
Gabriel Barbarin Gorostegui <gabriel.barbarin@clouddistrict.com>
2024-04-02 16:46:01 +0200

    Transform To Microservice

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,20 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { envs } from './config';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const logger = new Logger('main');
 
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        port: envs.port,
+      },
+    },
+  );
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -15,7 +24,6 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(envs.port);
-  logger.log(`App running on port ${envs.port}`);
+  logger.log(`Products Microservice running on port ${envs.port}`);
 }
 bootstrap();
